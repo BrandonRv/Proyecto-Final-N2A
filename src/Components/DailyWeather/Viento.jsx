ab3c3ba734612e26cf5d1b1c2803ccf7586e041d
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-10-24 09:33:51 -0500

    Update Viento.jsx

--- src/Components/DailyWeather/Viento.jsx
+++ src/Components/DailyWeather/Viento.jsx
@@ -1,22 +1,24 @@
 import React from "react";
+import useGeoLocation from '../Service/useGeoLocation';
 import './Viento.css';
 
 function Viento() {
+
+  const { current } = useGeoLocation();
+  const speedInKmph = (current?.wind?.speed * 3.6).toFixed(1);
+
   return (
     <>
       <div id="viento-0" className=" d-flex flex-column justify-content-center align-items-center">
-        <div style={{ fontSize: "12px" }}>Wind status</div>
+        <div>Wind status</div>
         <div className="container d-flex justify-content-center align-items-center gap-2">
-          <div className="valor-viento">Valor velocidad del Viento</div>
-          <div>mph</div>
+          <div className="valor-viento">{speedInKmph}</div>
+          <div>Km/h</div>
         </div>
         <div className="container d-flex justify-content-center align-items-center gap-2 ">
           <div
             style={{
-              transform: `rotate(${
-                90
-                // (dataWeather && dataWeather.wind.deg) - 90
-              }deg)`,
+              transform: `rotate(${(current?.wind?.deg)+ 105}deg)`,
               height: "25px",
               width: "25px",
             }}
