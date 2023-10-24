0f4af869a937c91a0bb3f4be68a828873770548f
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-10-24 18:57:18 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://brandonrv.github.io/Proyecto-Final-N2A/"
+  base: "/Proyecto-Final-N2A/"
 })
