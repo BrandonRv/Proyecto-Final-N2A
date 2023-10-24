2dd827adbfea1c34c184921f4edbe40764ed7666
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-10-24 09:31:52 -0500

    Update ClimaforDAY.jsx

--- src/Components/ClimaforDAY.jsx
+++ src/Components/ClimaforDAY.jsx
@@ -1,31 +1,47 @@
 import React from "react";
 import './ClimaforDAY.css';
 
-function ClimaforDAY() {
+function ClimaforDAY(props) {
+
+  console.log(props.fecha) // formato que arroja es asi: 2023-10-28 15:00:00
+  console.log(props.imagenPNG)
+  console.log(props.tempMax)
+  console.log(props.tempMin)
+  console.log(props.simbolo)
+
+  const fechaObj = new Date(props.fecha);
+  const diasSemana = ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'];
+  const diaSemana = diasSemana[fechaObj.getDay()];
+  const diaMes = fechaObj.getDate();
+  const meses = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+  const mes = meses[fechaObj.getMonth()];
+  // const fechaDiaria = {diaSemana, diaMes, mes}
+  
   return (
     <>
       <div
-        className="container m-0 d-flex flex-column justify-content-around align-items-center"
+        className="container m-2 d-flex flex-column justify-content-around align-items-center"
         style={{
           backgroundColor: "rgb(30, 33, 58)",
+          fontSize: "15px"
         }}
       >
+        {`${props.dia == undefined ? `${diaSemana}, ${diaMes} ${mes}` : props.dia}`}
         <div className="dias-semana">
-          DiaSem, DiaMes MES
         </div>
         <img
-        //   src={params.wheaterIMG}
+          src={props.imagenPNG}
           className="img-por-dia"
           alt="Weather.png"
         />
         <div className="temp-max-min">
-          <div className="d-flex">
-            <div>Temp Max</div>
-            <div>°C o °F</div>
+          <div className="d-flex p-2">
+            <div>{props.tempMax}</div>
+            <div>{props.simbolo}</div>
           </div>
           <div className="d-flex" style={{ color: "rgb(160, 159, 177)" }}>
-            <div>Temp Min</div>
-            <div>°C o °F</div>
+            <div>{props.tempMin}</div>
+            <div>{props.simbolo}</div>
           </div>
         </div>
       </div>
