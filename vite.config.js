141520f8ecec855c4c23d84a9859b4ae5cd512f8
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-10-24 17:19:58 -0500

    aplicacionclima

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://brandonrv.github.io/Proyecto-Final-N2A/"
 })
