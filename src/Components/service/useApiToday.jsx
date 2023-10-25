d69e712875afea9b8bb0091c48c07ebcf4d2aafc
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-10-25 15:03:00 -0500

    Update useApiToday.jsx

--- src/Components/service/useApiToday.jsx
+++ src/Components/service/useApiToday.jsx
@@ -1,26 +1,21 @@
 import { useState, useEffect } from 'react';
 
 function useApiToday() {
-  const [lat, setLat] = useState(null);
-  const [long, setLong] = useState(null);
+  const [latitude1, setLatitude1] = useState(null);
+  const [longitude1, setLongitude1] = useState(null);
   const [current, setCurrent] = useState(null);
 
-
-
-  const fetchWeatherData = async (latitude, longitude) => {
+  const fetchWeatherData = async (latitude1, longitude1) => {
     const KEY = "51b373d3e7fd4d6bf55f3c265f7f8cdd";
-    const link = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude == undefined ? "4.60971" : latitude}&lon=${longitude == undefined ? "-74.08175" : longitude}&appid=${KEY}`;
+    const link = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude1 == undefined ? "4.60971" : latitude1}&lon=${longitude1 == undefined ? "-74.08175" : longitude1}&appid=${KEY}`;
     const res = await fetch(link);
     const data = await res.json();
     setCurrent(data);
-    console.log(data)
   };
 
   const handleSuccess = (data) => {
-    const { latitude, longitude } = data.coords;
-    setLat(latitude);
-    setLong(longitude);
-    fetchWeatherData(latitude, longitude);
+    const { latitude1, longitude1 } = data.coords;
+    fetchWeatherData(latitude1, longitude1);
   };
 
   const handleError = () => {
@@ -28,13 +23,20 @@ function useApiToday() {
     fetchWeatherData()
   };
 
-
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
-  }, []);
+   
+    if(latitude1 === null && longitude1 === null) { 
+      navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+    } else {
+      fetchWeatherData(latitude1, longitude1)
+      console.log(latitude1)
+      console.log(longitude1)
+    }
+
+  }, [latitude1, longitude1]);
 
 
-  return { lat, long, current };
+  return { setLatitude1, setLongitude1, current };
 }
 
 export default useApiToday;
