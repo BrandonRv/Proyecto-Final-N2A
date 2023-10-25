c491b413cdebdebb7058214a37ae43470ed33889
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-10-25 00:02:58 -0500

    Update BarraState.jsx

--- src/Components/BarraState.jsx
+++ src/Components/BarraState.jsx
@@ -61,7 +61,7 @@ function BarraState() {
           alt="Cloud-background.png"
         />
         <img
-          src={`./${current?.weather[0]?.main}.png`}
+          src={`./img/${current?.weather[0]?.main}.png`}
           className="tiempo-Icon"
           alt="Weather.png"
         />
