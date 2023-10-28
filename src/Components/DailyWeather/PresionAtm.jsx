32c3e9226e1f3b22853b157c8f1191f229cf3804
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-10-27 22:43:18 -0500

    Update PresionAtm.jsx

--- src/Components/DailyWeather/PresionAtm.jsx
+++ src/Components/DailyWeather/PresionAtm.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import useGeoLocation from '../service/useGeoLocation';
+import { useWeatherContext } from '../../context/WeatherProvider';
 import './PresionAtm.css';
 
 function PresionAtm() {
-
-  const { current } = useGeoLocation();
-
+  const { current1 } =  useWeatherContext();
   return (
     <>
       <div id="atm-0" className=" d-flex flex-column justify-content-center align-items-center">
         <div>Air Pressure</div>
         <div className="container d-flex justify-content-center align-items-center gap-2">
           <div className="valor-atm">
-            {current?.main?.pressure}
-            {/* {dataWeather && dataWeather.main.pressure} */}
+            {current1?.main?.pressure}
           </div>
           <div>mb</div>
         </div>
